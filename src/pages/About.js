91e310a8480a64aee9f25fc03bbdab01a7625525
Johannes Bertens <johannes.bertens@gmail.com>
2017-05-04 17:43:27 +0200

    Updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,11 +7,12 @@ const About = () => (
         <section className="post">
             <header className="major">
                 <h1>About Techmusings</h1>
-                <p>This is a meta-post about this site and the author.</p>
+                <p>This is a meta-post about this site and the author: hi - this is me on the right!</p>
             </header>
-            <div className="image main"><img src="/images/pic01.jpg" alt="" /></div>
-            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sapien risus, commodo eget turpis at, elementum convallis enim turpis, lorem ipsum dolor sit amet nullam.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus. Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum.</p>
+            <div className="image main"><img src="/images/about.jpg" alt="image showing my loved one and myself" /></div>
+            <p>This picture is taken in Greece, during a great sunny day when my better half and I where there on holliday. We live in Rotterdam, The Netherlands (which is in Europe, for those vising from far far away) - so any sunny day anywhere is something to enjoy, haha!</p>
+            <p>I am working as a "Business and IT Architect" at Eneco, a large dutch Green Energy company. I also enjoy tinkering around with embedded systems, websites (like this one!), blockchain technology, home cinema and am a bit of a foodie.</p>
+            <p>This site serves two main purposes: <ol><li>To document and share cool new technologies and possibilitied with these technologies, and,</li><li>To learn React and how to tie together a React front-end to a ASP.NET Core backend, using Docker.</li></ol> I am also creating a course on this which should be available soon soon soon!</p>
         </section>
     </div>
 );
