1a6612106e8e4c9088131e3d23a794ac1005c753
Johannes Bertens <johannes.bertens@gmail.com>
2017-05-04 17:44:07 +0200

    Removed warnings

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => (
                 <h1>About Techmusings</h1>
                 <p>This is a meta-post about this site and the author: hi - this is me on the right!</p>
             </header>
-            <div className="image main"><img src="/images/about.jpg" alt="image showing my loved one and myself" /></div>
+            <div className="image main"><img src="/images/about.jpg" alt="showing my loved one and myself" /></div>
             <p>This picture is taken in Greece, during a great sunny day when my better half and I where there on holliday. We live in Rotterdam, The Netherlands (which is in Europe, for those vising from far far away) - so any sunny day anywhere is something to enjoy, haha!</p>
             <p>I am working as a "Business and IT Architect" at Eneco, a large dutch Green Energy company. I also enjoy tinkering around with embedded systems, websites (like this one!), blockchain technology, home cinema and am a bit of a foodie.</p>
             <p>This site serves two main purposes: <ol><li>To document and share cool new technologies and possibilitied with these technologies, and,</li><li>To learn React and how to tie together a React front-end to a ASP.NET Core backend, using Docker.</li></ol> I am also creating a course on this which should be available soon soon soon!</p>
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Footer from './Footer';
+// import Footer from './Footer';
 import PostFeatured from './PostFeatured';
 import Post from './Post';
 
