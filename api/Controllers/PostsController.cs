b4a498e84eeb2259611bb57c0d115eb82197efea
Johannes Bertens <johannes.bertens@gmail.com>
2017-05-06 11:48:18 +0200

    Database is up

--- api/Controllers/PostsController.cs
+++ api/Controllers/PostsController.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
+using System;
 
 namespace techmusings_api.Controllers
 {
     class Post
     {
+        public int Id;
         public string date;
         public string title;
         [JsonProperty(PropertyName = "abstract")] public string summary;
+        public string content;
         public string picture;
         public string link;
     }
@@ -17,32 +22,34 @@ namespace techmusings_api.Controllers
     public class PostsController : Controller
     {
         [HttpGet]
-        public JsonResult Get()
+        public async Task<JsonResult> Get()
         {
-            Post[] result = {
-                new Post
+            var posts = new List<Post>();
+
+            var conn = new MySqlConnection("server=db;user id=root;password=my-secret-pw;port=3306;database=techmusings;");
+            await conn.OpenAsync();
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = @"SELECT `Id`, `date`, `title`, `abstract`, `picture`, `link` FROM `posts` ORDER BY `Id` DESC LIMIT 10;";
+            
+            using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
                 {
-                    date = "April 28, 2017",
-                    title = "CI/CD inside Docker 3",
-                    summary = "Aenean ornare velit lacus varius enim ullamcorper proin aliquam facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor amet nullam sed etiam veroeros.",
-                    picture = "/images/ship1.jpg",
-                    link = "/article/docker-3"
-                }, new Post
-                {
-                    date = "April 24, 2017",
-                    title = "CI/CD inside Docker 2",
-                    summary = "Aenean ornare velit lacus varius enim ullamcorper proin aliquam facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor amet nullam sed etiam veroeros.",
-                    picture = "/images/pic02.jpg",
-                    link = "/article/docker-2"
-                }, new Post
-                {
-                    date = "April 22, 2017",
-                    title = "CI/CD inside Docker 1",
-                    summary = "Aenean ornare velit lacus varius enim ullamcorper proin aliquam facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor amet nullam sed etiam veroeros.",
-                    picture = "/images/pic02.jpg",
-                    link = "/article/docker-1"
-                }};
-            return Json(result);
+                    var post = new Post
+                    {
+                        Id = await reader.GetFieldValueAsync<int>(0),
+                        date = (await reader.GetFieldValueAsync<DateTime>(1)).ToString(),
+                        title = await reader.GetFieldValueAsync<string>(2),
+                        summary = await reader.GetFieldValueAsync<string>(3),
+                        picture = await reader.GetFieldValueAsync<string>(4),
+                        link = await reader.GetFieldValueAsync<string>(5)
+                    };
+                    posts.Add(post);
+                }
+            }
+
+            conn.Close();
+            return Json(posts);
         }
     }
 }
